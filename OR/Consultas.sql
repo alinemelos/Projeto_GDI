20e68a1193c51991daa39122c3e12127ce2d5625
Aline Marianna <meloaline703@gmail.com>
2024-09-25 22:11:15 -0300

    consultas finalizadas

--- OR/Consultas.sql
+++ OR/Consultas.sql
@@ -83,3 +83,83 @@ FROM
     ANUNCIA a
 JOIN 
     IMOVEL i ON a.IDIMOVEL = i.IDIMOVEL
+
+-- Seleciona nome, genero, endereço e lista os telefones de proprietarios
+SELECT nome, genero, LISTAGG(t.column_value, ', ') WITHIN GROUP(ORDER BY t.column_value)AS telefone, DEREF(endereco).rua AS rua,
+DEREF(endereco).cidade AS cidade
+FROM proprietario p, TABLE(p.telefone) t
+GROUP BY p.nome, p.genero, DEREF(p.endereco).rua, DEREF(p.endereco).cidade;
+
+-- Seleciona Imoveis que tem proprietário do sexo feminino
+SELECT idImovel, Valor, DEREF(i.endereco).cidade AS cidade
+FROM imovel i
+JOIN proprietario p ON i.cpf_proprietario = p.cpf_cliente
+WHERE p.genero ='Feminino';
+
+-- Seleciona nome do banco e lista os telefones para bancos que tem um financiamento com algum locatario
+SELECT  
+    b.nome,  
+    LISTAGG(t.column_value, ', ') WITHIN GROUP (ORDER BY t.column_value) AS telefone 
+FROM  
+    Banco b,  
+    TABLE(b.telefone) t
+WHERE b.cnpj IN (SELECT b.cnpj
+FROM Banco b 
+INNER JOIN financia f  
+    ON b.cnpj = DEREF(f.banco).cnpj
+INNER JOIN locatario l  
+    ON DEREF(f.locatario).cpf_cliente = l.cpf_cliente  
+) 
+GROUP BY  
+    b.nome
+
+--TESTANDO A FUNÇÃO valor_anual
+DECLARE
+  v_aluguel tipo_Aluga;
+  v_valor_anual FLOAT;
+BEGIN
+  -- Criando uma instância do tipo_aluga
+  v_aluguel := tipo_Aluga(idImovel => 123,
+                         CPF_corretor => '12345678901',
+                         CPF_locatario => '98765432101',
+                         Mensalidade => 1000,
+                         Inicio => TO_DATE('01/01/2024', 'DD/MM/YYYY'),
+                         Fim => TO_DATE('31/12/2024', 'DD/MM/YYYY'));
+
+  -- Calculando o valor anual
+  v_valor_anual := v_aluguel.valor_anual;
+
+  DBMS_OUTPUT.PUT_LINE('Valor anual: ' || v_valor_anual);
+END;
+/
+
+--Quais corretores contataram clientes que desejam uma casa
+SELECT c.Nome, l.Tipo_desejado
+FROM Corretor c
+INNER JOIN Contata co ON c.CPF_corretor = co.CPF_corretor
+INNER JOIN Locatario l ON co.CPF_locatario = l.CPF_cliente
+INNER JOIN Aluga a ON l.CPF_cliente = a.CPF_locatario
+INNER JOIN Imovel i ON a.idImovel = i.idImovel
+WHERE l.Tipo_desejado LIKE '%Casa%';
+
+-- Quais imóveis foram visitados por um determinado corretor e qual a avaliação média dessas visitas
+SELECT i.Titulo, AVG(v.Avaliacao) AS Media_Avaliacao
+FROM Imovel i
+INNER JOIN Visita v ON i.idImovel = v.idImovel
+WHERE v.CPF_corretor = '34567890123'  
+GROUP BY i.Titulo;
+
+--Quais imóveis receberam mais visitas em um determinado período
+SELECT i.Titulo, COUNT(*) AS Numero_Visitas
+FROM Imovel i
+INNER JOIN Visita v ON i.idImovel = v.idImovel
+WHERE v.Data BETWEEN '01-JAN-2023' AND '31-MAR-2024'  
+GROUP BY i.Titulo
+ORDER BY Numero_Visitas DESC;
+
+-- Quais clientes possuem um imóvel alugado
+SELECT c.Nome, COUNT(*) AS Numero_Imoveis_Alugados
+FROM Cliente c
+INNER JOIN Aluga a ON c.CPF_cliente = a.CPF_locatario
+GROUP BY c.Nome
+HAVING COUNT(*) = 1;
